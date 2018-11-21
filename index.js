30859712b7339a31d06faf6871e7ea9f48c97bc0
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-21 08:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function createRock(x) {
      } else if(top === 400) {
          GAME.removeChild(rock);
      } else {
-       rock.style.top += 20;
+       rock.style.top += `${top += 20}px`;
       //  if (top < 400) {
       //    window.requestAnimationFrame(moveRock);
       //   }
