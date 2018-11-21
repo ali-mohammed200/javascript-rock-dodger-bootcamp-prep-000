932fa3aa0b14241680730dcf9fec339bb27c325a
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-21 08:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function endGame() {
   for(let i = 0; i < ROCKS.length; i++){
     GAME.removeChild(ROCKS[i]);
   }
-
+  window.removeEventListener('keydown', moveDodger);
   alert(`YOU LOSE!`);
 }
 
