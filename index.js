c0c557504ccbd59d17e2584de3b9c143ba9f251d
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-21 08:18:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -131,10 +131,10 @@ function endGame() {
 
 function moveDodger(e) {
   // implement me!
-    if (e.which === 37) {
+    if (e.which === LEFT_ARROW) {
       moveDodgerLeft()
     }
-    else if(e.which === 39){
+    else if(e.which === RIGHT_ARROW){
       moveDodgerRight();
     }
   /**
