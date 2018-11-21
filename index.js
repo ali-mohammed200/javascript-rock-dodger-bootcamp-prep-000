257ecc2e7bb8da8fa764d44df57ec694cff1e2fb
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-21 09:02:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -166,8 +166,9 @@ function moveDodgerRight() {
   // implement me!
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
-  dodger.style.left = `${left + 4}px`
+
   if (left < 360) {
+    dodger.style.left = `${left + 4}px`
     window.requestAnimationFrame(moveDodgerRight);
   }
   /**
